72f6e95cf8b5fed6347eee632dff84409bebe745
ramalakshmi <ramalakshmi7761@gmail.com>
2026-07-17 15:21:21 +0530

    i change postmap

--- src/main/java/com/Bankingapplication/Bank/account/AccountController.java
+++ src/main/java/com/Bankingapplication/Bank/account/AccountController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AccountController {
 	@Autowired
 	private Accountinterface accountinter;
-	@PostMapping("/accountname3" )
+	@PostMapping("/accountname7" )
 	public AccountResponse createaccount(@RequestBody AccountRequest dto) {
 		return accountinter.createaccount(dto);
 	}
